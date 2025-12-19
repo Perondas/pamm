22d4b4d92d144f63a3af77def477414368e6f81a
Thomas <thomas@perondas.dev>
2025-12-19 14:42:16 +0100

    Renamed local_pack_config to pack_user_settings

--- lib/src/pack/pack_user_settings.rs
+++ lib/src/pack/pack_user_settings.rs
@@ -1,17 +1,17 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct LocalPackConfig {
+pub struct PackUserSettings {
     // TODO: add fields as needed
 }
 
-impl Default for LocalPackConfig {
+impl Default for PackUserSettings {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl LocalPackConfig {
+impl PackUserSettings {
     pub fn new() -> Self {
         Self {}
     }
