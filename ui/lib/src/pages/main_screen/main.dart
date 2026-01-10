1d6f5d9ed9075de500badc39efa70ba80d51ee23
Thomas <thomas@perondas.dev>
2026-01-10 15:38:34 +0100

    Extracted repo details to own widget

--- ui/lib/src/pages/main_screen/main.dart
+++ ui/lib/src/pages/main_screen/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ui/src/models/stored_repo.dart';
+import 'package:ui/src/pages/main_screen/repo_details/main.dart';
 import 'package:ui/src/pages/main_screen/repo_list/main.dart';
 
 class MainScreen extends StatefulWidget {
@@ -25,53 +26,7 @@ class _MainScreenState extends State<MainScreen> {
       body: Row(
         children: [
           RepoList(_onSelectRepo),
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.white,
-              child: _selectedRepo == null
-                  ? Center(child: Text("Select a repository to view details"))
-                  : Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _selectedRepo!.name,
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            const SizedBox(height: 8),
-                            Text(_selectedRepo!.description),
-                            const SizedBox(height: 12),
-                            Text(
-                              'Path:',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(_selectedRepo!.path),
-                            const SizedBox(height: 12),
-                            Text(
-                              'Packs:',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 8),
-                            if (_selectedRepo!.packs.isEmpty)
-                              Text('No packs available')
-                            else
-                              ..._selectedRepo!.packs.map(
-                                (p) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 2.0,
-                                  ),
-                                  child: Text('- $p'),
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-            ),
-          ),
+          RepoDetails(_selectedRepo),
         ],
       ),
     );
