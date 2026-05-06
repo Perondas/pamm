c6f81b7217e6b2726f7fb1a7840c8dceb076c92c
Thomas <thomas@perondas.dev>
2026-05-06 19:40:26 +0200

    Trying to increase RW performance

--- lib/src/io/serialization/serializers/bin_serializer.rs
+++ lib/src/io/serialization/serializers/bin_serializer.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
-use serde::de::DeserializeOwned;
 use serde::Serialize;
+use serde::de::DeserializeOwned;
 
 // Binary serialization using bincode
 
@@ -8,14 +8,17 @@ pub(crate) fn to_writer<E: Serialize, W: std::io::Write>(
     writer: &mut W,
     content: &E,
 ) -> anyhow::Result<()> {
-    bincode::serde::encode_into_std_write(content, writer, bincode::config::standard())?;
+    let mut buf_writer = std::io::BufWriter::new(writer);
+    bincode::serde::encode_into_std_write(content, &mut buf_writer, bincode::config::standard())?;
+    std::io::Write::flush(&mut buf_writer)?;
     Ok(())
 }
 
 pub(crate) fn from_reader<D: DeserializeOwned, R: std::io::Read>(
     reader: &mut R,
 ) -> anyhow::Result<D> {
+    let mut buf_reader = std::io::BufReader::new(reader);
     let res: Result<D, _> =
-        bincode::serde::decode_from_std_read(reader, bincode::config::standard());
+        bincode::serde::decode_from_std_read(&mut buf_reader, bincode::config::standard());
     res.context("Failed to deserialize binary data")
 }
--- lib/src/io/serialization/serializers/hr_serializer.rs
+++ lib/src/io/serialization/serializers/hr_serializer.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
-use serde::de::DeserializeOwned;
 use serde::Serialize;
+use serde::de::DeserializeOwned;
 
 // Human-readable JSON serialization
 
@@ -8,13 +8,16 @@ pub(crate) fn to_writer<E: Serialize, W: std::io::Write>(
     writer: &mut W,
     content: &E,
 ) -> anyhow::Result<()> {
-    serde_json::to_writer_pretty(writer, content)?;
+    let mut buf_writer = std::io::BufWriter::new(writer);
+    serde_json::to_writer_pretty(&mut buf_writer, content)?;
+    std::io::Write::flush(&mut buf_writer)?;
     Ok(())
 }
 
 pub(crate) fn from_reader<D: DeserializeOwned, R: std::io::Read>(
     reader: &mut R,
 ) -> anyhow::Result<D> {
-    let res: Result<D, _> = serde_json::from_reader(reader);
+    let mut buf_reader = std::io::BufReader::new(reader);
+    let res: Result<D, _> = serde_json::from_reader(&mut buf_reader);
     res.context("Failed to deserialize human-readable JSON data")
 }
