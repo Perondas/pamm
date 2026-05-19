1ba0cddb7fc8789ca08dbb787ca2e2f4b3bfb933
Thomas <thomas@perondas.dev>
2026-05-19 10:03:53 +0200

    Add functionality to apply and retrieve pack diffs with parents

--- lib/src/handle/actions/sync/mod.rs
+++ lib/src/handle/actions/sync/mod.rs
@@ -1,5 +1,7 @@
 pub mod apply_diff;
+pub mod apply_diffs_with_parents;
 pub mod config_sync_interactor;
 pub mod get_diff;
+pub mod get_diffs_with_parents;
 pub mod quick_check;
 pub mod sync_repo_config;
