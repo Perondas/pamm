cf304dcedae8c95ee21831a18b2313b2014e997d
Thomas <thomas@perondas.dev>
2026-06-18 13:48:37 +0200

    Removed unneeded impls

--- lib/src/handle/server_repo_handle.rs
+++ lib/src/handle/server_repo_handle.rs
@@ -1,9 +1,5 @@
-use crate::handle::reading::get_pack::GetPack;
-use crate::handle::reading::get_repo_info::GetRepoInfo;
 use crate::handle::repo_handle::RepoHandle;
 use crate::io::name_consts::WWW_DIR_NAME;
-use crate::models::pack::pack_config::PackConfig;
-use crate::models::pack::pack_user_settings::PackUserSettings;
 use crate::models::repo::repo_config::RepoConfig;
 use std::ops::{Deref, DerefMut};
 use std::path::{Path, PathBuf};
@@ -39,28 +35,5 @@ impl Deref for ServerRepoHandle {
 impl DerefMut for ServerRepoHandle {
     fn deref_mut(&mut self) -> &mut RepoHandle {
         &mut self.base
-    }
-}
-
-impl GetPack for ServerRepoHandle {
-    fn get_pack(&self, pack_name: &str) -> anyhow::Result<PackConfig> {
-        self.base.get_pack(pack_name)
-    }
-
-    fn get_pack_with_settings(
-        &self,
-        pack_name: &str,
-    ) -> anyhow::Result<(PackConfig, PackUserSettings)> {
-        self.base.get_pack_with_settings(pack_name)
-    }
-}
-
-impl GetRepoInfo for ServerRepoHandle {
-    fn get_repo_path(&self) -> &Path {
-        self.base.get_repo_path()
-    }
-
-    fn get_config(&self) -> &RepoConfig {
-        self.base.get_config()
     }
 }
